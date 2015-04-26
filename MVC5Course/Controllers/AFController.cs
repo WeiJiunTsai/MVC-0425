60a13b5aeae19b2e2fb25d2096cf4170f5543aae
WeiJiunTsai <antenna217@gmail.com>
2015-04-26 17:34:58 +0800

    透過額外的 [HandleError] 讓發生特定例外時顯示不同的錯誤頁面

--- MVC5Course/Controllers/AFController.cs
+++ MVC5Course/Controllers/AFController.cs
@@ -19,5 +19,18 @@ namespace MVC5Course.Controllers
             throw new Exception("test OK");
             return View();
         }
+        [HandleError(Master = "", ExceptionType = typeof(ArgumentException), View = "Error.Argument")]
+        public ActionResult MadeMeWrong2(string type = "")
+        {
+            if (type == "1")
+            {
+                throw new ArgumentException("OK");
+            }
+            else
+            {
+                throw new Exception("test OK");
+            }
+            return View();
+        }
     }
 }
