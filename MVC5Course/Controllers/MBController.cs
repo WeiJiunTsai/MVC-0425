8aeec4d3ef45d8ca08f40464faeb79d964de1930
WeiJiunTsai <antenna217@gmail.com>
2015-04-26 15:15:13 +0800

    練習 簡單&複雜 模型繫結

--- MVC5Course/Controllers/MBController.cs
+++ MVC5Course/Controllers/MBController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MVC5Course.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -33,5 +34,74 @@ namespace MVC5Course.Controllers
             ViewBag.ViewBag = "Test ViewBag";
             return View();
         }
+
+        public ActionResult Simple1()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Simple1(string username, string password)
+        {
+            return Content("Simple1 = Username:" + username + "," + "PASS:" + password);
+        }
+
+        public ActionResult Simple2()
+        {
+            return View("Simple1");
+        }
+        [HttpPost]
+        public ActionResult Simple2(FormCollection form)
+        {
+            return Content("Simple2 = Username:" + form["username"] + "," + "PASS:" + form["password"]);
+        }
+
+        public ActionResult Complex1()
+        {
+            return View("Simple1");
+        }
+        [HttpPost]
+        public ActionResult Complex1(SimpleViewModel item)
+        {
+            return Content("complex1 = USER: " + item.UserName +", Pass: "+ item.PassWord);
+        }
+
+        public ActionResult Complex2()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Complex2(SimpleViewModel item1,SimpleViewModel item2)
+        {
+            return Content("complex2 = USER1: " + item1.UserName +", Pass: "+ item1.PassWord +"; User2:" + item2.UserName+",Pass:"+item2.PassWord);
+        }
+
+        public ActionResult Complex3()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Complex3([Bind(Prefix = "item1")]SimpleViewModel item)
+        {
+            return Content("complex3 = USER: " + item.UserName + ", Pass: " + item.PassWord);
+        }
+
+        public ActionResult Complex4()
+        {
+            var data = from a in db.Client
+                       select new SimpleViewModel
+                       {
+                           UserName = a.FirstName,
+                           PassWord = a.LastName,
+                           age = 18
+                       };
+            return View(data.Take(10));
+        }
+        [HttpPost]
+        public ActionResult Complex4(IList<SimpleViewModel> item)
+        {
+            return Content("");
+        }
+
+        
     }
 }
---
+++ MVC5Course/Models/SimpleViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC5Course.Models
+{
+    public class SimpleViewModel: IValidatableObject
+    {
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string PassWord { get; set; }
+        [Required]
+        [Compare("PassWord")]
+        public string ComfirmPassWord { get; set; }
+        [Required]
+        public int age { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            yield break;
+        }
+    }
+}
