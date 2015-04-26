b2086095da5a131dd77a7436a3a8c403f2e13653
WeiJiunTsai <antenna217@gmail.com>
2015-04-26 10:07:21 +0800

    FileResult 練習

--- MVC5Course/Controllers/ARController.cs
+++ MVC5Course/Controllers/ARController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -31,6 +32,34 @@ namespace MVC5Course.Controllers
         public string Testcontent()
         {
             return "test content view. ";
+        }
+
+        public ActionResult File1()
+        {
+            byte[] content= System.IO.File.ReadAllBytes(Server.MapPath("~/Content/pic1.png"));
+            return File(content,"image/png");
+        }
+
+        public ActionResult File2()
+        {
+            return File(Server.MapPath("~/Content/pic1.png"),"image/png");
+        }
+
+        public ActionResult File3(string url)
+        {
+            var wc = new WebClient();
+            var content = wc.DownloadData(url);
+
+            return File(content,"image/png");
+        }
+        public ActionResult file4()
+        {
+            return File(Server.MapPath("~/Content/file4.htm"),"text/plain");
+        }
+        public ActionResult File5()
+        {
+            byte[] content = System.IO.File.ReadAllBytes(Server.MapPath("~/Content/pic1.png"));
+            return File(content, "image/png","pic5.png");
         }
     }
 }
