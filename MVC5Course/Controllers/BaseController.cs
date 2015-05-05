2e2f3b992a196fd1ca848186104f7a6e2b55cec0
WeiJiunTsai <antenna217@gmail.com>
2015-05-05 22:31:03 +0800

    驗證數字為偶數 & Authorize

---
+++ MVC5Course/Controllers/BaseController.cs
@@ -0,0 +1,28 @@
+﻿using MVC5Course.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+#if !DEBUG
+    [Authorize]
+#endif
+    public abstract class BaseController : Controller
+    {
+        protected FabricsEntities db = new FabricsEntities();
+
+        protected ClientRepository repo = RepositoryHelper.GetClientRepository();
+        protected OccupationRepository repoOccupation = RepositoryHelper.GetOccupationRepository();
+
+        #if Debug
+            public ActionResult debug()
+            {
+                return View();
+            }
+        #endif
+
+    }
+}
---
+++ MVC5Course/Models/evenIntAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC5Course.Models
+{
+    public class evenIntAttribute:DataTypeAttribute
+    {
+        public evenIntAttribute() : base("evenInt")
+        {
+            ErrorMessage = "數字需為偶數!";
+        }
+        public override bool IsValid(object value)
+        {
+            
+            if (value == null ) { return false; }
+
+            Decimal num;
+            if (Decimal.TryParse(value.ToString(), out num))
+            {
+                if(num != 0 && num % 2 == 0)
+                {
+                    return true;
+                }
+                else { return false; }
+            }
+            else { return false; }
+        }
+    }
+}
